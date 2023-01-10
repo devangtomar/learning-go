ecd01dc981b5496e397dfe89cb48d7842045f967
devangtomar <devangtomar123@gmail.com>
2023-01-10 17:05:17 +0530

    Adding maps!

---
+++ maps/map.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	var myMap map[string]float64 // That's how you declare a map in golang..
+	// With key as string and values as float64 with "map keyword"
+
+	var ranks map[string]int     // declaring a map variable
+	ranks = make(map[string]int) // actually create the map
+
+	ranks := make(map[string]int) // create a map and declare a variable to hold it
+}
