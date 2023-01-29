40926aef94bcf1aa3bac242fb57b20c206f5d734
devangtomar <devangtomar123@gmail.com>
2023-01-29 19:51:39 +0530

    Adding getters and setters!

---
+++ encapsulation/setters.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type Date struct {
+	Year  int
+	Month int
+	Day   int
+}
+
+func (d *Date) SetMonth(month int) error {
+	if month > 12 || month < 1 {
+		return errors.New("invalid month")
+	}
+	d.Month = month
+	return nil
+}
+
+func (d *Date) SetYear(year int) {
+	d.Year = year
+}
+
+func main() {
+	date := Date{}
+	date.SetYear(2019)
+	fmt.Println(date.Year)
+
+	// Adding validation to the setter methods
+	err := date.SetMonth(14)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(date.Month)
+}
