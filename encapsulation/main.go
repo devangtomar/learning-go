a42416716d1f3f34bc211d64299e5bb623575cf7
devangtomar <devangtomar123@gmail.com>
2023-01-30 19:54:25 +0530

    Adding Desc on getters/setters!

--- encapsulation/main.go
+++ encapsulation/main.go
@@ -1,3 +1,30 @@
+/*
+A method that is primarily used to set the value of an
+encapsulated field is known as a setter method. Setter
+methods often include validation logic, to ensure the new value
+being provided is valid.
+
+Since setter methods need to modify their receiver, their
+receiver parameter should have a pointer type.
+
+It’s conventional for setter method names to be in the form
+SetX where X is the name of the field being set.
+
+A method that is primarily used to get the value of an
+encapsulated field is known as a getter method.
+
+It’s conventional for getter method names to be in the form X
+where X is the name of the field being set. Some other
+programming languages favor the form GetX for getter method
+names, but you should not use that form in Go.
+
+Methods defined on an outer struct type live alongside methods
+promoted from an embedded type.
+
+An embedded type’s unexported methods don’t get promoted
+to the outer type.
+*/
+
 package main
 
 import (
