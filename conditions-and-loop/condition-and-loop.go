c96673263568bafe53d5e661b53d06f5335dd0ee
devangtomar <devangtomar123@gmail.com>
2023-10-18 01:08:15 +0530

    function vs methods

--- conditions-and-loop/condition-and-loop.go
+++ conditions-and-loop/condition-and-loop.go
@@ -15,7 +15,7 @@ func main() {
 func time_package() {
 	// this can also be written as .. year := time.Now().Year()
 	var now time.Time = time.Now()
-	var year int = now.Year()
+	var year int = now.Year() // `now.Year()` is a method call. `Year()` is a method of the `time.Time` type.  It is associated with the `now` variable, which is of type `time.Time`, and it returns the year of the time represented by `now`.
 
 	fmt.Println(year)
 }
@@ -24,8 +24,8 @@ func string_package() {
 	broken := "G# r#cks!"
 
 	// This can also be written as .. fixed := strings.NewReplacer("#", "o").Replace(broken)
-	replacer := strings.NewReplacer("#", "o")
-	fixed := replacer.Replace(broken)
+	replacer := strings.NewReplacer("#", "o") // this is a function..`strings.NewReplacer("#", "o")` is a call to a function named `NewReplacer` from the `strings` package. This function returns a new `*strings.Replacer` based on the arguments provided.
+	fixed := replacer.Replace(broken)         // this is a method since it returns a string type
 	fmt.Println(fixed)
 
 }
