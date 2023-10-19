f5355eb5299b21d159d60abe267ea1d5384f81fa
devangtomar <devangtomar123@gmail.com>
2023-10-20 00:35:43 +0530

    Understanding reflect and loops!

--- read-csv/csvStuff.go
+++ read-csv/csvStuff.go
@@ -4,7 +4,12 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	_ "reflect"
 )
+
+func main() {
+	print_csv()
+}
 
 func print_csv() {
 	// Open the CSV file
@@ -14,15 +19,25 @@ func print_csv() {
 		fmt.Println("File doesn't exists!")
 		return
 	}
-
 	reader := csv.NewReader(csv_file)
-
+	var i int = 0
+	// fmt.Println(reflect.TypeOf(reader))
 	for {
 		// Read a record from the CSV file
 		record, err := reader.Read()
 		if err != nil {
 			break
 		}
-		fmt.Println(record)
+		if i == 0 {
+			// break
+			goto next
+			// fmt.Println(reflect.TypeOf(record))
+		}
+	next:
+		// fmt.Println(record)
+		for _, value := range record {
+			// fmt.Println(index)
+			fmt.Println(value)
+		}
 	}
 }
