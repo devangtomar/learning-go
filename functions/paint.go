7444a779e8ee1ee70df2f88ce590f45d43790f5c
devangtomar <devangtomar123@gmail.com>
2023-11-05 22:32:40 +0530

    Calling functions multiple times!

--- functions/paint.go
+++ functions/paint.go
@@ -1,33 +1,16 @@
-// ! The rules for function names are the same as the rules for variable names:
-
-// * A name must begin with a letter, followed by any number of
-// * additional letters and numbers. (You’ll get a compile error if you
-// * break this rule.)
-
-// * Functions whose name begins with a capital letter are exported,
-// * and can be used outside the current package. If you only need to
-// * use a function inside the current package, you should start its name
-// * with a lowercase letter.
-
-// * Names with multiple words should use camelCase
-
 package main
 
 import (
 	"fmt"
 )
 
-func sayHi() {
-	fmt.Println("Hi!")
-}
-
 func main() {
-	sayHi()
-	repeatLine("hey there!", 10)
+	paintNeeded(4.2, 3.0)
+	paintNeeded(1.6, 1.0)
+	paintNeeded(3.3, 8.0)
 }
 
-func repeatLine(line string, times int) {
-	for i := 0; i < times; i++ {
-		fmt.Println(line)
-	}
+func paintNeeded(width float64, height float64) {
+	area := width * height
+	fmt.Printf("%.2f litres needed\n", area)
 }
