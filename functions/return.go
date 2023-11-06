3a3241569f90bb9a7ea00f444ee31dcdaf2a8e07
devangtomar <devangtomar123@gmail.com>
2023-11-06 22:37:56 +0530

    Functions return type!

--- functions/return.go
+++ functions/return.go
@@ -6,9 +6,18 @@ func double(number float64) float64 {
 	return number * 2
 }
 
+func status(grade float32) string {
+	if grade < 60.0 {
+		return "Failing!"
+	}
+	return "Passing!"
+}
+
 func main() {
 	var dozen float64
 	dozen = double(6.0)
 	fmt.Println(dozen)
 	fmt.Println(double(4.2))
+	fmt.Println(status(60.1))
+	fmt.Println(status(59))
 }
