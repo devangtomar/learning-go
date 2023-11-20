8bc512e14e90fc0153ca19245d1e293571b42df6
devangtomar <devangtomar123@gmail.com>
2023-11-20 23:56:56 +0530

    Change variable using pass-by-value

--- functions/pointers.go
+++ functions/pointers.go
@@ -21,6 +21,10 @@ func pointer() {
 	fmt.Println(&amount) // retrieves the variable's address!
 }
 
+func printPointer(myBoolPointer *bool) {
+	fmt.Println(*myBoolPointer)
+}
+
 func main() {
 	amount := 6
 	double(amount)
@@ -42,14 +46,27 @@ func main() {
 	fmt.Println(myIntPointer)
 
 	// Short declaration for a pointer variable!
-	var myNewBool bool =  false
+	var myNewBool bool = false
 	myBoolPointer := &myNewBool
 	fmt.Println(myBoolPointer)
 
 	// For printing value of a pointer!
 	fmt.Println(*myBoolPointer)
 
+	// Using pointers with functions
+
 	var myFloatPointer *float64 = createPointer()
 	fmt.Println(*myFloatPointer)
 
+	var myFuncBool bool = true
+	printPointer(&myFuncBool)
+
+	// Fixing our “double” function using pointers ~> this will now change the original value of amt variable!
+	amt := 7
+	doublePointerFix(&amt)
+	fmt.Println(amt)
+}
+
+func doublePointerFix(number *int) {
+	*number *= 2
 }
