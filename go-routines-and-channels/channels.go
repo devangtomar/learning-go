eb4f438b038bfc2f6105771348131f6ff25bf9fc
devangtomar <devangtomar123@gmail.com>
2024-01-22 00:49:12 +0530

    Adding more on channels and go-routines!

---
+++ go-routines-and-channels/channels.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func greeting(myChannel chan string) { // function taking argument as channel for values with a type string
+	myChannel <- "hi" // Passing "hi" string into channel
+}
+
+func main() {
+	myChannel := make(chan string)
+	go greeting(myChannel)   // That how you call the function with a newly created channel
+	// fmt.Println(<-myChannel) // Passing the incoming channel into print function!
+
+	// We could also do this..
+	recievedVar := <-myChannel
+	fmt.Println(recievedVar)
+}
