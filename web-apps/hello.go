ba7be3c79dccde630294a01daf9afefd943b950c
devangtomar <devangtomar123@gmail.com>
2024-02-05 22:23:01 +0530

    Adding mroe on web-pages!

--- web-apps/hello.go
+++ web-apps/hello.go
@@ -5,16 +5,27 @@ import (
 	"net/http"
 )
 
-func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	message := []byte("Hello web!")
-	_, err := writer.Write(message)
+func write(writer http.ResponseWriter, message string) {
+	_, err := writer.Write([]byte(message))
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func englishHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Hello, web!")
+}
+func frenchHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Hello, web!")
+}
+func hindiHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Hello, web!")
+}
+
 func main() {
-	http.HandleFunc("/hello", viewHandler)
+	http.HandleFunc("/hello", englishHandler)
+	http.HandleFunc("/salut", frenchHandler)
+	http.HandleFunc("/namaste", hindiHandler)
 	log.Println("Server is running on http://localhost:8080")
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
