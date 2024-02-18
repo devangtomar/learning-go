f8a1cdf4bbfcd5873ef1eabbd4940c34df73c9ca
devangtomar <devangtomar123@gmail.com>
2024-02-18 21:40:20 +0530

    update helloWorld.go

--- helloWorld.go
+++ helloWorld.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	files, err := os.ReadDir(".")
+	if err != nil {
+		fmt.Println("Error encountered!", err)
+		return
+	}
+	fmt.Println("All the files in current dir : ", files)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter a number!")
+	text, _ := reader.ReadString('\n')
+
+	text = strings.TrimSuffix(text, "\n")
+
+	fmt.Println("You entered the following string : ", text)
 }
